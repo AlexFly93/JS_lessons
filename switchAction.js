5dedbf6604eee81640ad816e2e1cf1c846d67144
Alexandr <muhabon93@gmail.com>
2024-03-25 18:56:16 +0200

    Add tasks description

--- switchAction.js
+++ switchAction.js
@@ -1,15 +1,18 @@
+//Задача 2: Выбор действия (решается с помощью switch-case)
+//Задание: У вас есть строка с действием ("добавить", "вычесть", "умножить", "разделить") и два числа. Напишите блок кода, выполняющего выбранное действие с этими числами и выводящий результат.
+
 function calculate(action, num1, num2) {
   switch (action) {
-      case "добавить":
-          return num1 + num2;
-      case "вычесть":
-          return num1 - num2;
-      case "умножить":
-          return num1 * num2;
-      case "разделить":
-          return num1 / num2;
-      default:
-          return "error";
+    case "добавить":
+      return num1 + num2;
+    case "вычесть":
+      return num1 - num2;
+    case "умножить":
+      return num1 * num2;
+    case "разделить":
+      return num1 / num2;
+    default:
+      return "error";
   }
 }
 

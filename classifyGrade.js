eac2497ca6d2a34c8ead6d9655b64606ffbaddf8
Alexandr <muhabon93@gmail.com>
2024-03-26 10:44:35 +0200

    add error catching to classifyGrade

--- classifyGrade.js
+++ classifyGrade.js
@@ -3,16 +3,17 @@
 
 function classifyGrade(score) {
   if (score >= 90 && score <= 100) {
-      return "Отлично";
+    return "Отлично";
   } else if (score >= 70 && score <= 89) {
-      return "Хорошо";
+    return "Хорошо";
   } else if (score >= 50 && score <= 69) {
-      return "Удовлетворительно";
-  } else if (score < 50) {
-      return "Неудовлетворительно";
-  } 
+    return "Удовлетворительно";
+  } else if (score < 50 && score > 0) {
+    return "Неудовлетворительно";
+  } else {
+    return "incorrect score"
+  }
 }
 
-
-let score = 85; 
+const score = 101;
 console.log(classifyGrade(score));
